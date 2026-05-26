e3995318f4def1a9d9388d39c80c289ad0679d2f
Ankit! <rajakankit7815@gmail.com>
2026-05-26 22:00:30 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,17 +1,42 @@
 import {useState} from 'react'
+import Home from './Home'
+// import Home from './Home'
+import {New} from './Home'
+
  const App = () => {
+  let user="ankitt"
     const [count,setCount]= useState(0)
     function fun1(){
         setCount(count + 1)
         
     }
+    function fun2(){
+      setCount(count - 1)
+    }
+    
+
   return (
     <div>
-    <h2>{count}
-    </h2>
-    <button onClick={fun1}>Click</button>
+    {/* <h2>{count}
+    </h2> */}
+    {/* <button onClick={fun1}>Click + </button>
+    <button onClick={fun2}>Click-</button> */}
+    
+      {/* <Home/> */}
+      <Home data={user}/>
+      <New/>
     </div>
   )
-}
+// import React from 'react'
+// import Home from './Home'
+
+
+
+ }
 
 export default App
+
+
+
+
+// export default App
---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = ({data}) => {
+    console.log(data)
+  return (
+    <div>Home </div>
+  )
+}
+const New = () =>{
+    return(
+        <div>neww filee</div>
+    )
+}
+export {New} 
+
+export default Home
